b4dc2f4d3e7478dd9d3c3492713c3d8379c2e01b
akshayDatacode <akshay.webllisto@gmail.com>
2019-06-19 14:27:40 +0530

    update: models and templates

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -5,5 +5,5 @@ urlpatterns=[
 
 path('index',views.index,name='index'),
 path('stdlogin',login.stdlogin,name='stdlogin'),
-
+path('article_list',views.article,name='article_list'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("HELLO AKSHAY STARING SOMETHING NEW FOR OUR PROJECT ")
+
+
+
+def article(request):
+	article = Article.objects.all()
+	return render(request,'article_list.html', {'article':article})
+		
